91ed9615457d2fccba0adc845abfb69f0d19765c
Patrick Schwehm <patrick.schwehm@saitow.ag>
2025-03-06 12:53:42 +0100

    Clean up

--- lua/patrick/packer.lua
+++ lua/patrick/packer.lua
@@ -1,37 +1,59 @@
+-- Ensure packer.nvim is loaded
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
 	-- Packer can manage itself
 	use("wbthomason/packer.nvim")
+
+	-- === UI Enhancements ===
+	use("ellisonleao/gruvbox.nvim") -- Color scheme
+	use("nvim-lualine/lualine.nvim") -- Status line
+	use("Yggdroot/indentLine") -- Show indentation guides
+	use("ya2s/nvim-cursorline") -- Highlights the current line and word
 	use({
-		"ThePrimeagen/harpoon",
+		"anuvyklack/pretty-fold.nvim", -- Better code folding
+		config = function()
+			require("pretty-fold").setup()
+		end,
+	})
+	use("gregsexton/MatchTag") -- Highlight matching HTML/XML tags
+
+	-- === Navigation & File Management ===
+	use("preservim/nerdtree") -- File tree
+	use("christoomey/vim-tmux-navigator") -- Seamless navigation between Tmux and Vim
+	use({
+		"ThePrimeagen/harpoon", -- Quick navigation between files
 		branch = "harpoon2",
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 
-	use("ellisonleao/gruvbox.nvim")
-	use("mbbill/undotree")
-	use("terrortylor/nvim-comment")
-	use("lewis6991/gitsigns.nvim")
-	use("windwp/nvim-autopairs")
-	use("gregsexton/MatchTag")
-	use("Yggdroot/indentLine")
-	use("mattn/emmet-vim")
-	use("nvim-lualine/lualine.nvim")
+	-- === Terminal ===
 	use({
-		"akinsho/toggleterm.nvim",
+		"akinsho/toggleterm.nvim", -- Floating terminal inside Neovim
 		tag = "*",
 		config = function()
 			require("toggleterm").setup()
 		end,
 	})
-  use {
-  "ibhagwan/fzf-lua",
-  requires = { "nvim-tree/nvim-web-devicons" }, -- Optional for icons
-  config = function()
-    require("fzf-lua").setup({})
-  end
-}
+
+	-- === Fuzzy Finder (fzf-lua) ===
+	use({
+		"ibhagwan/fzf-lua",
+		requires = { "nvim-tree/nvim-web-devicons" }, -- Optional for icons
+		config = function()
+			require("fzf-lua").setup({})
+		end,
+	})
+
+	-- === Git Integration ===
+	use("lewis6991/gitsigns.nvim") -- Git signs in the gutter
+	use({
+		"kdheepak/lazygit.nvim", -- LazyGit integration
+		requires = { "nvim-lua/plenary.nvim" },
+	})
+	use("APZelos/blamer.nvim") -- Git blame inline annotations
+
+	-- === LSP, Autocompletion, and Snippets ===
 	use({
 		"VonHeikemen/lsp-zero.nvim",
 		branch = "v1.x",
@@ -54,28 +76,23 @@ return require("packer").startup(function(use)
 			{ "rafamadriz/friendly-snippets" },
 		},
 	})
-	-- formatting & linting
-	use("jose-elias-alvarez/null-ls.nvim")
-	use("jayp0521/mason-null-ls.nvim")
 
-	use("christoomey/vim-tmux-navigator")
-	use("preservim/nerdtree")
-	use("echasnovski/mini.nvim")
-	use({
-		"anuvyklack/pretty-fold.nvim",
-		config = function()
-			require("pretty-fold").setup()
-		end,
-	})
-	use("mtth/scratch.vim")
-	use({
-		"kdheepak/lazygit.nvim",
-		-- optional for floating window border decoration
-		requires = {
-			"nvim-lua/plenary.nvim",
-		},
-	})
-	use("echasnovski/mini.surround")
+	-- === Formatting & Linting ===
+	use("jose-elias-alvarez/null-ls.nvim") -- Linter/Formatter
+	use("jayp0521/mason-null-ls.nvim") -- Mason integration for null-ls
+
+	-- === Productivity & Utilities ===
+	use("mbbill/undotree") -- Undo history visualizer
+	use("terrortylor/nvim-comment") -- Easy comment toggling
+	use("windwp/nvim-autopairs") -- Auto-close brackets and quotes
+	use("echasnovski/mini.nvim") -- Lightweight collection of plugins
+	use("echasnovski/mini.surround") -- Better surround text editing
+	use("mtth/scratch.vim") -- Scratchpad for quick notes
+
+	-- === Search & Navigation ===
+	use("rlane/pounce.nvim") -- Jump to words quickly
+
+	-- === Notifications & Messages ===
 	use({
 		"folke/noice.nvim",
 		requires = {
@@ -83,12 +100,21 @@ return require("packer").startup(function(use)
 			{ "rcarriga/nvim-notify" },
 		},
 	})
-	use("APZelos/blamer.nvim")
-	use("rlane/pounce.nvim")
-  use("ya2s/nvim-cursorline")
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
+	use({
+		"kdheepak/lazygit.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+	})
+	use("echasnovski/mini.surround") -- Quick surround edits
+	use({
+		"anuvyklack/pretty-fold.nvim",
+		config = function()
+			require("pretty-fold").setup()
+		end,
+	})
+
+	-- === Automatically set up packer after cloning ===
 	if packer_bootstrap then
 		require("packer").sync()
 	end
 end)
+
--- lua/patrick/remap.lua
+++ lua/patrick/remap.lua
@@ -1,43 +1,62 @@
+-- Set the leader key to space
 vim.g.mapleader = " "
-local opts = {}
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- General options table
+local opts = {}
+
+-- File explorer
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open file explorer" })
+
+-- Terminal escape to normal mode
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", opts)
 
--- splits
-vim.keymap.set("n", "<leader>w", "<C-w><C-w>", opts)
-vim.keymap.set("n", "<leader>vs", ":vsplit<CR>", opts)
-vim.keymap.set("n", "<leader>hs", ":split<CR>", opts)
-vim.keymap.set("n", "<leader>+", ':exe "resize " . (winheight(0) * 3/2)<CR>', opts)
-vim.keymap.set("n", "<leader>-", ':exe "resize " . (winheight(0) * 2/3)<CR>', opts)
+-- Window navigation
+vim.keymap.set("n", "<leader>h", "<C-w>h", opts) -- Move to left window
+vim.keymap.set("n", "<leader>l", "<C-w>l", opts) -- Move to right window
+vim.keymap.set("n", "<leader>j", "<C-w>j", opts) -- Move to below window
+vim.keymap.set("n", "<leader>k", "<C-w>k", opts) -- Move to above window
+vim.keymap.set("n", "<leader>w", "<C-w><C-w>", opts) -- Cycle through windows
 
-vim.keymap.set("n", "<leader>*", ':exe "vertical resize " . (winwidth(0) * 3/2)<CR>', opts)
-vim.keymap.set("n", "<leader>_", ':exe "vertical resize " . (winwidth(0) * 2/3)<CR>', opts)
+-- Splitting windows
+vim.keymap.set("n", "<leader>vs", ":vsplit<CR>", opts) -- Vertical split
+vim.keymap.set("n", "<leader>hs", ":split<CR>", opts) -- Horizontal split
 
+-- Resize splits
+vim.keymap.set("n", "<leader>*", "<cmd>vertical resize +5<CR>", opts) -- Increase width
+vim.keymap.set("n", "<leader>_", "<cmd>vertical resize -5<CR>", opts) -- Decrease width
+vim.keymap.set("n", "<leader>+", "<cmd>resize +5<CR>", opts) -- Increase height
+vim.keymap.set("n", "<leader>-", "<cmd>resize -5<CR>", opts) -- Decrease height
+
+-- Equalize vertical splits
+vim.keymap.set("n", "<leader>=", ":lua Equalize_vertical_splits()<CR>", { noremap = true, silent = true, desc = "Equalize vertical splits" })
+
+-- Reload current Lua config file
 vim.keymap.set("n", "<leader><CR>", ":luafile %<CR>", opts)
 
-vim.keymap.set("n", "<leader>l", "<C-w>l", opts)
-vim.keymap.set("n", "<leader>h", "<C-w>h", opts)
-vim.keymap.set("n", "<leader>j", "<C-w>j", opts)
-vim.keymap.set("n", "<leader>k", "<C-w>k", opts)
+-- Clipboard integration (yank to system clipboard)
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to clipboard" })
 
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<C-n>", ":NERDTreeToggle<CR>")
+-- Toggle NERDTree file explorer
+vim.keymap.set("n", "<C-n>", ":NERDTreeToggle<CR>", { desc = "Toggle NERDTree" })
 
-vim.keymap.set("n", "<leader>t", ":lua search_by_filetype()<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>=", ":lua Equalize_vertical_splits()<CR>", { noremap = true, silent = true })
+-- Searching by filetype
+vim.keymap.set("n", "<leader>t", ":lua search_by_filetype()<CR>", { noremap = true, silent = true, desc = "Search by filetype" })
 
-vim.keymap.set("n", "<leader>n", ":nohls<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { noremap = true, silent = true })
+-- Clear search highlighting
+vim.keymap.set("n", "<leader>n", ":nohls<CR><Esc>", { noremap = true, silent = true, desc = "Clear search highlight" })
 
+-- Git integrations
+vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { noremap = true, silent = true, desc = "Open LazyGit" })
+vim.keymap.set("n", "<leader>gs", require("fzf-lua").git_status, { desc = "fzf-lua git status" })
+
+-- Noice message dismiss
 vim.keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice Message" })
 
-vim.keymap.set("n", "<leader>p", "<cmd>Pounce<CR>", { desc = "Pounce" })
+-- Pounce (fuzzy search)
+vim.keymap.set("n", "<leader>p", "<cmd>Pounce<CR>", { desc = "Pounce fuzzy search" })
 
--- fzf-lua
-vim.keymap.set("n", "<leader>f", require("fzf-lua").files, {desc = "fzf-lua"})
-vim.keymap.set("n", "<leader>F", require("fzf-lua").live_grep, {desc = "fzf-lua live grep"})
-vim.keymap.set("n", "<leader>r", require("fzf-lua").resume, {desc = "fzf-lua resume"})
-vim.keymap.set("n", "<leader>b", require("fzf-lua").buffers, {desc = "fzf-lua buffers"})
-vim.keymap.set("n", "<leader>gs", require("fzf-lua").git_status, {desc = "fzf-lua git status"})
-
+-- fzf-lua key mappings
+vim.keymap.set("n", "<leader>f", require("fzf-lua").files, { desc = "fzf-lua find files" })
+vim.keymap.set("n", "<leader>F", require("fzf-lua").live_grep, { desc = "fzf-lua live grep" })
+vim.keymap.set("n", "<leader>r", require("fzf-lua").resume, { desc = "fzf-lua resume search" })
+vim.keymap.set("n", "<leader>b", require("fzf-lua").buffers, { desc = "fzf-lua buffers" })
